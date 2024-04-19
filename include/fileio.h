acc8f29b62a78bd85691de0664d63c119d9298b3
ratgrandmother <118036429+RatGrandmother@users.noreply.github.com>
2024-04-19 13:23:27 +0000

    Finished read_file, edited main

--- include/fileio.h
+++ include/fileio.h
@@ -6,15 +6,25 @@
 
 char* read_file(char* buffer) {
     FILE* file_p = fopen(buffer, "r");
-    char* contents = (char*)malloc(sizeof(char));
-
+    char* contents;
     if (file_p == NULL) {
         printf("ERRRRR: Could not open file %s\n", buffer);
         return NULL;
     }
 
-    
+    fseek(file_p, 0, SEEK_END);
+    long file_size = ftell(file_p);
+    rewind(file_p);
 
+    contents = (char*)malloc(file_size * sizeof(char));
+    fread(contents, sizeof(char), file_size, file_p);
+
+    contents[file_size] = '\0';
+
+    return contents;
+
+    // cleanup
+    free(contents);
     fclose(file_p);
 }
 
--- src/main.c
+++ src/main.c
@@ -4,13 +4,13 @@
 #include "../include/fileio.h"
 
 int main(int argc, char** argv) {
-
-    if (argv[1] < 2) {
+    if (argc < 2) {
         printf("USAGE: %s <filename.c8>", argv[0]);
         return EXIT_FAILURE;
     }
+    printf("%s\n", read_file(argv[1]));
 
-    read_file(argv[1]);
+
 
     return EXIT_SUCCESS;
 }

b6a1bb5d330a62b95352bc051378f90fbba4dfce
ratgrandmother <118036429+RatGrandmother@users.noreply.github.com>
2024-04-19 13:44:48 +0000

    Included some headers.

---
+++ src/parser.c
@@ -0,0 +1,10 @@
+#include <string.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+
+// initalize a new scanner
+// use the scanner 
+// scan the file
+// return instruction with Vx and Vy
+// run it in the instructions.c file
